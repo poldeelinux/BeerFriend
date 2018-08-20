bec358e621a07d8b26c61cf31bbd6f22fb3222c1
WanchalermPoldee <wanchalerm.it@gmail.com>
2018-08-20 16:59:18 +0700

    Finish 1 day OK

--- app/src/main/java/beer/sru/ac/th/beerfriend/RegisterFragment.java
+++ app/src/main/java/beer/sru/ac/th/beerfriend/RegisterFragment.java
@@ -1,15 +1,25 @@
 package beer.sru.ac.th.beerfriend;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class RegisterFragment extends Fragment{
+
+//    Explicit
+    private ImageView imageView;
+
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -18,7 +28,45 @@ public class RegisterFragment extends Fragment{
 //        Create Toolbar
         createToolbar();
 
+//        Avata Controller
+        avataController();
+
     }//Main Method
+
+    private void avataController() {
+        imageView = getView().findViewById(R.id.imvAvata);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent,"Please Choose Photo"),2);
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId()==R.id.itemUpload) {
+
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_register,menu);
+
+
+    }
 
     private void createToolbar() {
         Toolbar toolbar = getActivity().findViewById(R.id.toolbarRegister);
@@ -38,6 +86,8 @@ public class RegisterFragment extends Fragment{
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
+
+        setHasOptionsMenu(true);
 
     }
 
