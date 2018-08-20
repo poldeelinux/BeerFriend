07b5451371b3f715825238fb9a5aabac59eaa4b0
WanchalermPoldee <wanchalerm.it@gmail.com>
2018-08-20 16:20:05 +0700

    Toolbar Success OK

--- app/src/main/java/beer/sru/ac/th/beerfriend/RegisterFragment.java
+++ app/src/main/java/beer/sru/ac/th/beerfriend/RegisterFragment.java
@@ -4,11 +4,42 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+    }//Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+//        SetUp Title
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.register);
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Fill All Every Blank");
+
+//        Setup Navigation
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+    }
 
     @Nullable
     @Override
