72c4e4d4f58d8521a60ec148a760911f92054cc7
WanchalermPoldee <wanchalerm.it@gmail.com>
2018-08-20 13:29:02 +0700

    Replace Fragment OK

--- app/src/main/java/beer/sru/ac/th/beerfriend/MainFragment.java
+++ app/src/main/java/beer/sru/ac/th/beerfriend/MainFragment.java
@@ -7,8 +7,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+        registerController();
+
+
+    }//Main Method
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Replace Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentFragmentMain, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
 
     @Nullable
     @Override
