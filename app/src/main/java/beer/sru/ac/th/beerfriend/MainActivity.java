b4ab358c742ae6e71cd667dcd9b16d21057d182b
WanchalermPoldee <wanchalerm.it@gmail.com>
2018-08-20 11:35:26 +0700

    MainFragment OK

--- app/src/main/java/beer/sru/ac/th/beerfriend/MainActivity.java
+++ app/src/main/java/beer/sru/ac/th/beerfriend/MainActivity.java
@@ -9,5 +9,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if(savedInstanceState==null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentFragmentMain,new MainFragment())
+                    .commit();
+        }
+
+
+    }//Main Method
+
+}//Main Class
