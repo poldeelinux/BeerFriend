bdc8dc8664185c7172eb99079bc350630d807a62
WanchalermPoldee <wanchalerm.it@gmail.com>
2018-08-20 10:28:42 +0700

    Initial commit

---
+++ app/src/main/java/beer/sru/ac/th/beerfriend/MainActivity.java
@@ -0,0 +1,13 @@
+package beer.sru.ac.th.beerfriend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
